0f5d2d45087f11a9a07cab09d92953b76365e16c
leafar-Santos <leafarccomputacao@gmail.com>
2026-09-11 19:16:37 -0300

    Ajuste navegação

--- src/components/layout/AppShell/AppShell.jsx
+++ src/components/layout/AppShell/AppShell.jsx
@@ -1,9 +1,25 @@
-import { Outlet } from 'react-router-dom'
+import { useLayoutEffect } from 'react'
+import { Outlet, useLocation } from 'react-router-dom'
 import TopBar from '../TopBar/TopBar'
 import BottomNav from '../BottomNav/BottomNav'
 import './AppShell.css'
 
 export default function AppShell() {
+  const location = useLocation()
+
+  useLayoutEffect(() => {
+    const previousRestoration = window.history.scrollRestoration
+    window.history.scrollRestoration = 'manual'
+
+    return () => {
+      window.history.scrollRestoration = previousRestoration
+    }
+  }, [])
+
+  useLayoutEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'instant' })
+  }, [location])
+
   return (
     <div className="app-shell">
       <TopBar />
